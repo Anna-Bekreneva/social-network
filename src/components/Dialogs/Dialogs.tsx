9c6dd00ad8226f0ee1fa1b63ec13e3c0176c0c6a
Anna-Bekreneva <anjabekreneva@gmail.com>
2022-11-23 11:18:18 +0300

    lesson 40

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import s from './Dialogs.module.css'
 import {Message} from './Message/Message';
-import {dialogElements, StoreType} from '../../redux/state';
+import {
+	addPostActionCreator,
+	sendMessageActionCreator,
+	StoreType,
+	updateMessageBodyActionCreator
+} from '../../redux/state';
+import {DialogItem} from './DialogItem/DialogItem';
 
 
 export const Dialogs: React.FC<StoreType> = (props) => {
-	let messagesElements = props._state.dialogsPage.dialogs.map( message => <Message message={message.name}></Message> )
+	const dialogElements = props._state.dialogsPage.dialogs.map(dialog => <DialogItem name={dialog.name} id={dialog.id}/>);
+	const messagesElements = props._state.dialogsPage.messages.map( message => <Message message={message.message}></Message> )
+	const newMessageBody = props._state.dialogsPage.newMessageBody
+
+	const onSendMessageClick = () => {
+		props.dispatch(sendMessageActionCreator())
+	}
+
+	const onNewMessageChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+		props.dispatch(updateMessageBodyActionCreator(event.currentTarget.value))
+	}
+
 	return (
 		<div className={s.dialogs}>
 			<ul className={s.dialogs_items}>
 				{dialogElements}
 			</ul>
 			<div className={s.messages}>
-				{messagesElements}
+				<div>{messagesElements}</div>
+				<div>
+					<textarea value={newMessageBody} onChange={onNewMessageChange} placeholder={'Enter your message'}></textarea>
+					<button onClick={onSendMessageClick}>Send</button>
+				</div>
 			</div>
 		</div>
 	)
--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -1,6 +1,6 @@
-import {DialogItem} from '../components/Dialogs/DialogItem/DialogItem';
 import React from 'react';
 import {rerenderEntireTree} from '../index';
+import {text} from 'stream/consumers';
 
 export type StoreType = {
 	_state: StateType
@@ -21,7 +21,6 @@ export const store: StoreType = {
 			],
 			newPostText: ''
 		},
-
 		dialogsPage: {
 			dialogs: [
 				{id: 1, name: 'Dimych'},
@@ -37,47 +36,77 @@ export const store: StoreType = {
 				{id: 2, message: 'How is your it-kamasutra'},
 				{id: 3, message: 'Yo'},
 				{id: 4, message: 'Yo'},
-			]
+			],
+			newMessageBody: ''
 		},
 		sidebar: {}
 	},
 	_callSubscriber() {
-		console.log('state changed')
+		console.log('state changed');
 	},
 	getState() {
 		return this._state;
 	},
-	subscribe(callback){
-		this._callSubscriber = callback
+	subscribe(callback) {
+		this._callSubscriber = callback;
 	},
 	dispatch(action) {
 		if (action.type === 'ADD-POST') {
-			const newPost= {
+			const newPost: PostType = {
 				id: 5,
 				message: action.postText,
 				likesCount: 0
 			};
-			this._state.profilePage.posts.push(newPost)
-			this._state.profilePage.newPostText = ''
-			rerenderEntireTree()
-		} else if (action.type === 'UPDATE-NEW-POST-TEXT') {
-			this._state.profilePage.newPostText = action.newText
-			rerenderEntireTree()
-		}
+			this._state.profilePage.posts.push(newPost);
+			this._state.profilePage.newPostText = '';
+			rerenderEntireTree();
+		} else
+			if (action.type === 'UPDATE-NEW-POST-TEXT') {
+				this._state.profilePage.newPostText = action.newText;
+				rerenderEntireTree();
+			} else
+				if (action.type === 'UPDATE-NEW-MESSAGE-BODY') {
+					this._state.dialogsPage.newMessageBody = action.body;
+					rerenderEntireTree();
+				} else
+					if (action.type === 'SEND-MESSAGE') {
+						const body = this._state.dialogsPage.newMessageBody
+						this._state.dialogsPage.newMessageBody = '';
+						const newMessage: MessageType = {
+							id: 6,
+							message: body,
+						};
+						this._state.dialogsPage.messages.push(newMessage);
+						console.log(this._state.dialogsPage.messages)
+						rerenderEntireTree();
+					}
 	}
-}
+};
 
 export const addPostActionCreator = (text: string) => ({
 	type: 'ADD-POST',
 	postText: text
-} as const)
+} as const);
 
 export const updatePostTextActionCreator = (text: string) => ({
 	type: 'UPDATE-NEW-POST-TEXT',
 	newText: text
-} as const)
+} as const);
 
-type ActionsType = ReturnType<typeof addPostActionCreator> | ReturnType<typeof updatePostTextActionCreator>
+export const updateMessageBodyActionCreator = (text: string) => ({
+	type: 'UPDATE-NEW-MESSAGE-BODY',
+	body: text
+} as const);
+
+export const sendMessageActionCreator = () => ({
+	type: 'SEND-MESSAGE',
+} as const);
+
+type ActionsType =
+	ReturnType<typeof addPostActionCreator> |
+	ReturnType<typeof updatePostTextActionCreator> |
+	ReturnType<typeof updateMessageBodyActionCreator> |
+	ReturnType<typeof sendMessageActionCreator>
 
 type SidebarType = {}
 
@@ -105,6 +134,7 @@ export type ProfilePageType = {
 type DialogPageType = {
 	dialogs: Array<DialogType>
 	messages: Array<MessageType>
+	newMessageBody: string
 }
 
 export type StateType = {
@@ -112,5 +142,3 @@ export type StateType = {
 	dialogsPage: DialogPageType
 	sidebar: SidebarType
 }
-
-export const dialogElements = store._state.dialogsPage.dialogs.map(dialog => <DialogItem name={dialog.name} id={dialog.id}/>);
