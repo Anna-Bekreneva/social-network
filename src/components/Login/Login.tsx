c3a44f5d3b4cce70d3985f05ae1b169195e1a7ef
Anna-Bekreneva <anjabekreneva@gmail.com>
2023-10-30 19:11:10 +0300

    add 99 lesson

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Field, InjectedFormProps, reduxForm} from "redux-form";
+import {Field, reduxForm} from "redux-form";
 import {Input} from "components/common/FormsControls/FormsControls";
 import {required} from "utils/validators/validators";
 import {connect} from "react-redux";
