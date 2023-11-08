c7abcad9288c8c78eaa7bc6fa4025030431ce340
Anna-Bekreneva <anjabekreneva@gmail.com>
2023-11-08 19:37:07 +0300

    correct FormControls and validators

--- src/components/common/FormsControls/FormsControls.tsx
+++ src/components/common/FormsControls/FormsControls.tsx
@@ -1,26 +1,26 @@
 import React from "react";
 
-import { WrappedFieldProps } from "redux-form";
+import { Field, WrappedFieldMetaProps, WrappedFieldProps } from "redux-form";
 
 import styles from "./FormControls.module.css";
+import { FieldValidatorType } from "../../../utils";
 
-type TextareaProps3 = WrappedFieldProps & {
-  children: any;
+type FormControlPropsType = {
+  meta: WrappedFieldMetaProps;
 };
 
-export const FormControl = ({ input, meta, children, ...props }: TextareaProps3) => {
-  const hasError = meta.error && meta.touched;
-
+const FormControl: React.FC<FormControlPropsType> = ({ meta: { touched, error }, children }) => {
+  const hasError = touched && error;
   return (
     <div className={styles.formControl + " " + (hasError ? styles.error : "")}>
       {children}
-      {hasError && <span>{meta.error}</span>}
+      {hasError && <span>{error}</span>}
     </div>
   );
 };
 
-export const Textarea = (props: TextareaProps3) => {
-  const { input, meta, children, ...restProps } = props;
+export const Textarea: React.FC<WrappedFieldProps> = (props) => {
+  const { input, meta, ...restProps } = props;
 
   return (
     <FormControl {...props}>
@@ -29,7 +29,7 @@ export const Textarea = (props: TextareaProps3) => {
   );
 };
 
-export const Input = (props: TextareaProps3) => {
+export const Input: React.FC<WrappedFieldProps> = (props) => {
   const { input, meta, ...restProps } = props;
 
   return (
@@ -38,3 +38,18 @@ export const Input = (props: TextareaProps3) => {
     </FormControl>
   );
 };
+export function createField<FormKeysType extends string>(
+  placeholder: string | undefined,
+  name: FormKeysType,
+  validators: Array<FieldValidatorType>,
+  component: React.FC<WrappedFieldProps>,
+  props = {},
+  text = "",
+) {
+  return (
+    <div>
+      <Field placeholder={placeholder} name={name} validate={validators} component={component} {...props} />
+      {text}
+    </div>
+  );
+}
--- src/utils/validators/validators.ts
+++ src/utils/validators/validators.ts
@@ -1,11 +1,14 @@
-export const required = (value: string) => {
+export type FieldValidatorType = (value: string) => string | undefined;
+export const required: FieldValidatorType = (value) => {
   if (value) return undefined;
 
   return "field is required";
 };
 
-export const maxLengthCreator = (maxLength: number) => (value: string) => {
-  if (value.length > maxLength) return "Max length is 30 symbols";
+export const maxLengthCreator =
+  (maxLength: number): FieldValidatorType =>
+  (value) => {
+    if (value.length > maxLength) return "Max length is 30 symbols";
 
-  return undefined;
-};
+    return undefined;
+  };
