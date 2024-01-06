6102c14903020bdb989b130efbd172966720c0d5
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-01-06 19:13:45 +0300

    11 lesson of part 2

---
+++ src/components/Navigation/NavigationContainer.tsx
@@ -0,0 +1,30 @@
+import React, { ComponentPropsWithoutRef } from "react";
+
+import { connect } from "react-redux";
+
+import { AppStateType, logout } from "store";
+import { Navigation } from "./";
+
+type MapStatePropsType = {
+  name: string | null;
+  ava: string | null;
+};
+
+type MapDispatchToPropsType = {
+  logout: () => void;
+};
+
+class NavigationInner extends React.Component<NavigationPropsType> {
+  render() {
+    const { ava, name, logout, ...restProps } = this.props;
+    return <Navigation ava={ava} name={name} logout={logout} {...restProps} />;
+  }
+}
+
+const mapStateToProps = (state: AppStateType): MapStatePropsType => ({
+  name: state.profile.profile.fullName,
+  ava: state.profile.profile.photos.small,
+});
+
+export type NavigationPropsType = MapStatePropsType & MapDispatchToPropsType & ComponentPropsWithoutRef<"nav">;
+export const NavigationContainer = connect(mapStateToProps, { logout })(NavigationInner);
