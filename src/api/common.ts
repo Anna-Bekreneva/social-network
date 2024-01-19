d263021ea5911021514c696a0df7cecfb1622fc7
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-01-19 19:48:48 +0300

    change API-KEY

--- src/api/common.ts
+++ src/api/common.ts
@@ -4,7 +4,7 @@ export const instance = axios.create({
   withCredentials: true,
   baseURL: "https://social-network.samuraijs.com/api/1.0/",
   headers: {
-    "API-kEY": "f7924403-26c6-4ed7-ac40-d679f3c4cd6f",
+    "API-kEY": "758506e2-acfc-4428-b496-ba8aaa722b57",
   },
 });
 
